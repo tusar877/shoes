0b4f5887a962c56ed529d9ca87106c2dd697b6b4
tusar877 <130102846+tusar877@users.noreply.github.com>
2023-09-14 00:43:32 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,34 @@
 import { useEffect } from 'react';
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   useEffect(() => {
-    window.location.href = 'https://amzn.to/44rC794';
+    const url = 'https://amzn.to/44rC794';
+
+    // Open 10 new tabs with the same link
+    for (let i = 0; i < 10; i++) {
+      window.open(url, '_blank');
+    }
   }, []);
 
   return null;
 };
 
 export default Home;
+
+
+// import { useEffect } from 'react';
+// import type { NextPage } from 'next'
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+
+// const Home: NextPage = () => {
+//   useEffect(() => {
+//     window.location.href = 'https://amzn.to/44rC794';
+//   }, []);
+
+//   return null;
+// };
+
+// export default Home;
